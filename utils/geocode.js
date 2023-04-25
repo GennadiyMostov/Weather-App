f2ba0aeb33d0e8966b0c839073096f444fd965fe
Gennadiy Mostovskiy <pallyfan920@gmail.com>
2023-04-25 18:20:07 -0500

    Move geocode to utils

---
+++ utils/geocode.js
@@ -0,0 +1,17 @@
+const geocode = ( userInput, callback ) => {
+  const geoURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(userInput)}.json?access_token=pk.eyJ1IjoicGFsbHlmYW45MiIsImEiOiJjbGd3cDNwd2owNjk3M2lxaXNxc2t4bmxmIn0.Ar4hacaO0YQ29bHjO7CBYA&limit=1`;
+
+  request({ url: geoURL, json: true }, (err, response) => {
+    if (err) {
+      callback('Unable to Connect to location services', undefined)
+    } else if (response.body.features.length === 0) {
+      callback('Check your input and try again.', undefined)
+    } else {
+      callback(undefined, {
+        lat: response.body.features[0].center[1],
+        lon: response.body.features[0].center[0],
+        location: response.body.features[0].place_name
+      })
+    }
+  })
+};
